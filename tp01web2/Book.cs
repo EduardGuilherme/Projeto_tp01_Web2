78383f51424bb5397e178ffffde271ee191ab153
EduardGuilherme <eduguilherme48@gmail.com>
2020-09-20 23:01:56 -0300

    Trabalho em andamento

---
+++ tp01web2/Book.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+namespace tp01web
+{
+    class Book
+    {
+        
+        public string Name { get; set; }
+        public Author[] Authors { get; set; }
+        public double Price { get; set; }
+        public int Qty { get; set; }
+
+        public Book(string _name,Author[] _author,double _price)
+        {
+            Name = _name;
+            Authors = _author;
+            Price = _price;
+            Qty = 0;
+           
+        }
+        public Book(string _name, Author[] _author, double _price,int _qty)
+        {
+            Name = _name;
+            Authors = _author;
+            Price = _price;
+            Qty = _qty;
+        }
+
+        public override string ToString()
+        {
+            string aux = "";
+
+            foreach (Author a in Authors)
+            {
+
+                aux += "\n"+a.Name + "|"+a.Email+"|"+a.Gender+"|\n";
+
+
+            }
+          
+            return $" \n{Name}\n{aux}|{Price}|{Qty}";
+        }
+        public string getAuthorNames()
+        {
+            string aux ="";
+            
+            foreach (Author a in Authors)
+            {
+
+                aux += a.Name+"-";
+                
+              
+            }
+            return aux;
+                  
+
+        }
+    }
+}
---
+++ tp01web2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+using tp01web;
+
+namespace tp01web2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Author[] at = new Author[2];
+            Author a = new Author("Gabriel", "gabriel@paciencia", 'm');
+            Author a1 = new Author("eduardo", "Eduardo@eduardo", 'm');
+            at.SetValue(a, 0);
+            at.SetValue(a1, 1);
+            Book b = new Book("qualquerlivro",at,2.00,0);
+            Console.WriteLine(b.ToString());
+            
+
+            Console.ReadKey();
+        }
+    }
+}
